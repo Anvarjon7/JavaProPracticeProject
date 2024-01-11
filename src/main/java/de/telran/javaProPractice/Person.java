f00dbf1850593a2d210b271e94c42e109295f6e3
Win-10 <azimovanvarjon777@gmail.com>
2024-01-11 15:30:01 +0100

    Input and OutPut ex

--- src/main/java/de/telran/javaProPractice/Person.java
+++ src/main/java/de/telran/javaProPractice/Person.java
@@ -3,6 +3,23 @@ package de.telran.javaProPractice;
 public class Person {
     private String name;
     private int age;
+    private double height;
+    private boolean married;
+
+    public Person(String name, int age, double height, boolean married) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.married = married;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
 
     public Person(String name, int age) {
         this.name = name;
